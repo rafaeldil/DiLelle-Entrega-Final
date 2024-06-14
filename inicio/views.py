9d6ac5d2bddc37dbd03b3bb7b7e3cd1c95f8d261
rafadil <rafaeldilelle98@gmail.com>
2024-06-14 13:54:31 -0300

    Ultimos cambios

--- inicio/views.py
+++ inicio/views.py
@@ -2,28 +2,25 @@ from django.shortcuts import render, redirect
 from inicio.forms import CrearClienteForm, CrearLibroForm
 from inicio.models import Libro, Cliente
 
-# Create your views here.
-
+# View para la página de inicio
 def inicio(request):
     return render(request, 'inicio/index.html')
 
-
+# View para crear un libro
 def crear_libro(request):
     if request.method == 'POST':
-        form =  CrearLibroForm(request.POST)
+        form = CrearLibroForm(request.POST)
         if form.is_valid():
             datos = form.cleaned_data
             libro = Libro(nombre=datos.get('nombre'), genero=datos.get('genero'), precio=datos.get('precio'))
             libro.save()
-            
-# def crear_cliente(request):
-#     if request.method == 'POST':
-#         form =  CrearClienteForm(request.POST)
-#         if form.is_valid():
-#             datos = form.cleaned_data
-#             cliente = Cliente(nombre=datos.get('nombre'), direccion=datos.get('direccion'), telefono=datos.get('telefono'))
-#             cliente.save()
+            return redirect('lista_libros') 
+    else:
+        form = CrearLibroForm()
 
+    return render(request, 'inicio/crear_libro.html', {'form': form})
+
+# View para crear un cliente
 def crear_cliente(request):
     if request.method == 'POST':
         form = CrearClienteForm(request.POST)
@@ -32,19 +29,17 @@ def crear_cliente(request):
             cliente = Cliente(nombre=datos.get('nombre'), direccion=datos.get('direccion'), telefono=datos.get('telefono'))
             cliente.save()
             return redirect('inicio') 
+    else:
+        form = CrearClienteForm()
 
     return render(request, 'inicio/crear_cliente.html', {'form': form})
 
-            
+# View para listar libros
 def lista_libros(request):
-    
     libros = Libro.objects.all()
-    
     return render(request, 'inicio/lista_libros.html', {'libros': libros})
 
-
+# View para listar clientes
 def lista_clientes(request):
-
     clientes = Cliente.objects.all()
-    
     return render(request, 'inicio/lista_clientes.html', {'clientes': clientes})
