616b12e2f813674bad4d143cae6ed8f40576906f
rafadil <rafaeldilelle98@gmail.com>
2024-07-01 22:05:35 -0300

    CRUD Terminado

--- productos/views.py
+++ productos/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from .models import Paleta
 from django.urls import reverse_lazy
@@ -20,7 +20,15 @@ class CrearPaleta(CreateView):
     fields = ['marca', 'descripcion', 'fecha']
    
 class EditarPaleta(UpdateView):
-    ...    
+    model = Paleta
+    template_name = 'paletas/editar_paleta.html'
+    success_url = reverse_lazy('paletas')
+    fields = ['marca', 'descripcion', 'fecha']
+    
+class EliminarPaleta(DeleteView):
+    model = Paleta
+    template_name = "paletas/eliminar_paleta.html"
+    success_url = reverse_lazy('paletas')
     
 class VerPaleta(DetailView):
     model = Paleta
